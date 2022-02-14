aac8d317c44e63ed6184efedba28fa618654fd9a
yenner2025 <yenner2025@hotmail.com>
2022-02-13 21:45:45 -0500

    imple...

--- app/View/Components/Sidebar.php
+++ app/View/Components/Sidebar.php
@@ -23,9 +23,85 @@ class Sidebar extends Component
      */
     public function render()
     {
-        $nombre_usuario = "Juan Perez";
+        $titulo_virtual_stats = 'Consultoría Virtual';
+        $nombre_usuario = auth()->user()->name;
+        $opciones_menu = [
+            "Actor A" => [
+                [
+                    'titulo' => 'Consultas',
+                    'ruta' => 'categoria.index'
+                ],
+                [
+                    'titulo' => 'Actualizaciones',
+                    'ruta' => 'categoria.index'
+                ],
+                [
+                    'titulo' => 'Estadísticas',
+                    'ruta' => 'categoria.index'
+                ],
+            ],
+                "Actor B" => [
+                    [
+                        'titulo' => 'Consultas',
+                        'ruta' => 'categoria.index'
+                    ],
+                    [
+                        'titulo' => 'Actualizaciones',
+                        'ruta' => 'categoria.index'
+                    ],
+                    [
+                        'titulo' => 'Estadísticas',
+                        'ruta' => 'categoria.index'
+                    ],
+                ],
+                "Actor C" => [
+                    [
+                        'titulo' => 'Consultas',
+                        'ruta' => 'categoria.index'
+                    ],
+                    [
+                        'titulo' => 'Actualizaciones',
+                        'ruta' => 'categoria.index'
+                    ],
+                    [
+                        'titulo' => 'Estadísticas',
+                        'ruta' => 'categoria.index'
+                    ],
+                ],
+                "Administración" =>[
+                [
+                    'titulo' => 'Usuarios',
+                    'ruta' => 'usuarios.index'
+                ],
+                [
+                    'titulo' => 'Permisos',
+                    'ruta' => 'permisos.index'
+                ],
+            ],
+
+        ];
+
+        $opciones_dash = [
+            [
+                'titulo' => 'Dashboard',
+                'activo' => 'active',
+                'extra' => 'Dash',
+                'ruta' => 'dashboard.index'
+            ],
+            [
+                'titulo' => 'Estadísticas Gral.',
+                'activo' => '',
+                'extra' => 'Stats',
+                'ruta' => 'estadisticas.index'
+            ],
+        ];
+
+
         return view('components.sidebar',[
-            "usuario" => $nombre_usuario,
+            'titulo' => $titulo_virtual_stats,
+            'nombre_usuario' => $nombre_usuario,
+            'menu' => $opciones_menu,
+            'op_dash' => $opciones_dash, 
         ]);
     }
 }
--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -3,7 +3,7 @@
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
       <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">AdminLTE 3</span>
+      <span class="brand-text font-weight-light">{{ $titulo }}</span>
     </a>
 
     <!-- Sidebar -->
@@ -14,7 +14,7 @@
           <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block">{{ $usuario }}</a>
+          <a href="#" class="d-block">{{ $nombre_usuario }}</a>
         </div>
       </div>
 
@@ -35,38 +35,39 @@
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
           <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
-          <li class="nav-item menu-open">
-            <a href="#" class="nav-link active">
+               @foreach ($op_dash as $key1 => $value1)
+               <li class="nav-item">
+                 <a href="#" class="nav-link {{ $value1['activo']}}">
+                   <i class="nav-icon fas fa-th"></i>
+                   <p>
+                     {{ $value1['titulo']}}
+                     <span class="right badge badge-danger">{{ $value1['extra']}}</span>
+                   </p>
+                 </a>
+               </li>
+               @endforeach
+
+               @foreach($menu as $nombre_modulo => $opciones)
+          <li class="nav-item">
+            <a href="#" class="nav-link">
               <i class="nav-icon fas fa-tachometer-alt"></i>
               <p>
-                Bienvenido
+                {{ $nombre_modulo }}
                 <i class="right fas fa-angle-left"></i>
               </p>
             </a>
             <ul class="nav nav-treeview">
-              <li class="nav-item">
-                <a href="#" class="nav-link active">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Active Page</p>
-                </a>
-              </li>
+              @foreach ($opciones as $key2 => $value2)
               <li class="nav-item">
                 <a href="#" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
-                  <p>Inactive Page</p>
+                  <p>{{ $value2['titulo'] }}</p>
                 </a>
               </li>
+              @endforeach
             </ul>
           </li>
-          <li class="nav-item">
-            <a href="#" class="nav-link">
-              <i class="nav-icon fas fa-th"></i>
-              <p>
-                Simple Link
-                <span class="right badge badge-danger">New</span>
-              </p>
-            </a>
-          </li>
+          @endforeach
         </ul>
       </nav>
       <!-- /.sidebar-menu -->
